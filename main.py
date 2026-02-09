abdd2ea31c3e8a65730eddc289285ba1c94057a1
Thomas Bourke <thomas.bourke1@outlook.com>
2026-02-09 13:46:35 +0000

    update intro message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from cl-sem-analyzer!")
+    print("\nHello from cl-sem-analyzer! \nThis package was written by me to plot cathodoluminescence and SEM data. \nSee \scripts to run individual workflows. \nIf you have any questions, email me at tgb42@cam.ac.uk\n\n")
 
 
 if __name__ == "__main__":
